5839e39e6df240ada822da5f10188ca5889f12bf
Chris Wickens <chris@otree.org>
2018-02-06 16:45:02 +0000

    Release v2.0.21 (imported from PyPI)

--- otree/project_template/manage.py
+++ otree/project_template/manage.py
@@ -6,5 +6,5 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-    from otree_startup import execute_from_command_line
+    from otree.management.cli import execute_from_command_line
     execute_from_command_line(sys.argv, script_file=__file__)
