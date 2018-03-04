96b98ea17e056dfe3f84c54c8c405ad705669bcb
Chris Wickens <chris@otree.org>
2018-03-04 16:38:24 +0000

    Release v2.0.28 (imported from PyPI)

--- otree/management/commands/startproject.py
+++ otree/management/commands/startproject.py
@@ -2,9 +2,6 @@ import os
 from django.core.management.commands import startproject
 from django.core.management.base import CommandError
 import sys
-
-import six
-
 import otree
 from otree_startup import pypi_updates_cli
 
@@ -12,33 +9,22 @@ from otree_startup import pypi_updates_cli
 class Command(startproject.Command):
     help = ("Creates a new oTree project.")
 
-    def add_arguments(self, parser):
-        super().add_arguments(parser)
-        ahelp = (
-            'Tells the command to NOT prompt the user for '
-            'input of any kind.')
-        parser.add_argument(
-            '--noinput', action='store_false', dest='interactive',
-            default=True, help=ahelp)
-
     def handle(self, *args, **options):
-        if options["interactive"]:
-            answer = None
-            while not answer or answer not in "yn":
-                answer = six.moves.input("Include sample games? (y or n): ")
-                if not answer:
-                    answer = "y"
-                    break
-                else:
-                    answer = answer[0].lower()
-        else:
-            answer = 'n'
-        if answer == "y":
+        if os.path.isfile('settings.py') and os.path.isfile('manage.py'):
+            self.stdout.write(
+                'You are trying to create a project but it seems you are '
+                'already in a project folder (found settings.py and manage.py).'
+            )
+            sys.exit(-1)
+
+        answer = input("Include sample games? (y or n): ")
+        if answer and answer[0].lower() == "y":
             project_template_path = (
                 "https://github.com/oTree-org/oTree/archive/master.zip")
         else:
             project_template_path = os.path.join(
                 os.path.dirname(otree.__file__), 'project_template')
+
         if options.get('template', None) is None:
             options['template'] = project_template_path
         try:
