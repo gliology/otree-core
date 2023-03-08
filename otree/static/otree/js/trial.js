72c44497facbf747f231bcaeb01ee45640fcf526
Chris Wickens <chris@otree.org>
2023-03-08 18:19:13 +0000

    Release v6.0.0a2 (imported from PyPI)

--- otree/static/otree/js/trial.js
+++ otree/static/otree/js/trial.js
@@ -1,19 +1,7 @@
-var _trialSocket;
-
 function makeTrialSocket() {
   var $currentScript = $('#otree-trial');
   var socketUrl = $currentScript.data('socketUrl');
   return makeReconnectingWebSocket(socketUrl);
 }
 
-_trialSocket = makeTrialSocket();
-
-
-// prevent form submission when user presses enter in an input
-$(document).ready(function() {
-  $('input').on('keypress', function (e) {
-      if (e.key === 'Enter') {
-          e.preventDefault();
-      }
-  });
-});
+window._trialSocket = makeTrialSocket();
